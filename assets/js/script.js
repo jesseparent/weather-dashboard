d4f498a6ab7fe7cee239c0b8eef6b3f06b136b0e
Jesse Parent <jesseparent@gmail.com>
2020-09-04 22:47:37 -0600

    HTML wired to Javascript results error handling for bad API requests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,139 @@
-// Get Weather from Open Weather API
-let weatherOpen = "d29753543671bbb32ab67b6cd97cac2e";
-let lat = "";
-let long = "";
+// Store Open Weather API key
+let openWeatherKey = "d29753543671bbb32ab67b6cd97cac2e";
 
-let fetchWeather = function () {
+let todayGoodResult = $(".today").html();
+let fiveDayGoodResult = $("#fiveDayCards").html();
+
+let fetchWeather = function (city) {
   // Get today's weather and UV Index in imperial units
-  fetch("http://api.openweathermap.org/data/2.5/weather?q=Salt%20Lake%20City&appid=" + weatherOpen + "&units=imperial")
+  fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + openWeatherKey + "&units=imperial")
     .then(function (weatherResponse) {
-      return weatherResponse.json();
+      if (weatherResponse.ok) {
+        return weatherResponse.json();
+      }
+      else {
+        // The API call for Open Weather's Current Weather API did not return as successful
+        throw "Weather";
+      }
     })
     .then(function (weatherObj) {
       displayWeather(weatherObj);
-      return fetch("https://api.openweathermap.org/data/2.5/onecall?exclude=minutely,hourly&units=imperial&appid=" + weatherOpen + "&lat=" + weatherObj.coord.lat + "&lon=" + weatherObj.coord.lon)
+      return fetch("https://api.openweathermap.org/data/2.5/onecall?exclude=minutely,hourly&units=imperial&appid=" + openWeatherKey + "&lat=" + weatherObj.coord.lat + "&lon=" + weatherObj.coord.lon);
     })
-    .then(function (oneCallResonse) {
-      return oneCallResonse.json();
+    .then(function (oneCallResponse) {
+      if (oneCallResponse.ok) {
+        return oneCallResponse.json();
+      }
+      else {
+        // The API call for Open Weather's One Call API did not return as successful
+        throw "OneCall";
+      }
     })
     .then(function (oneCallObj) {
       displayUVI(oneCallObj)
       displayFiveDay(oneCallObj);
+    })
+    .catch(function (error) {
+      // Error thrown
+      let today = $(".today");
+      let fiveDay = $("#fiveDayCards");
+
+      if (error === "Weather") {        
+        today.html("<h2>Cannot Obtain Data for that Query</h2>");
+        today.css("visibility", "visible");
+      }
+      else if (error === "OneCall") {        
+        fiveDay.html("<h2>No 5 Day Forecast for that Query</h2>");
+        fiveDay.css("visibility", "visible");
+      }
+      else {
+        today.html("<h2>Failed to Reach API</h2>");
+        today.css("visibility", "visible");
+      }
     });
 };
 
-// Display weather for today
+// Display weather and date for today
 let displayWeather = function (dataObj) {
+  // City and date details
   let cityName = dataObj.name;
   let todaysDate = moment(parseInt(dataObj.dt) * 1000).format("M/DD/YYYY");
-  console.log(cityName + " (" + todaysDate + ")");
   let weatherIcon = dataObj.weather[0].icon;
-  console.log("http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
 
+  $("#cityDate").html(cityName + ' (' + todaysDate + ')  <img class="wi" src="http://openweathermap.org/img/wn/' + weatherIcon + '@2x.png">');
+
+  // Weather details for city
   let temperature = dataObj.main.temp;
-  console.log("Temperature: " + temperature + " °F");
   let humidity = dataObj.main.humidity;
-  console.log("Humidity: " + humidity + "%");
   let windSpeed = dataObj.wind.speed;
-  console.log("Wind Speed: " + windSpeed + " MPH");
+
+  $("#temperature").text(temperature);
+  $("#humidity").text(humidity);
+  $("#windSpeed").text(windSpeed);
+
+  // Show today's forecast
+  $(".today").css("visibility", "visible");
 };
 
 // Display UV Index for today
 let displayUVI = function (uviObj) {
   let uvIndex = uviObj.current.uvi;
-  let condition = (uvIndex < 6)? ((uvIndex < 3)? "favorable" : "moderate") : "severe";
-  console.log("UV Index: " + uvIndex + " - " + condition);
+  let uvIndexObj = $("#uvIndex");
+
+  uvIndexObj.text(uvIndex);
+
+  // Determine if UV Index is favorable (under 3.0), moderate (under 6.0), or severe (6.0 or more)
+  //  as determined by the EPA at https://www.epa.gov/sunsafety/uv-index-scale-0
+  let condition = (uvIndex < 6) ? ((uvIndex < 3) ? "bg-success" : "bg-warning") : "bg-danger";
+
+  $("#uvIndex").removeClass("bg-success bg-warning bg-danger");
+  uvIndexObj.addClass(condition);
 };
 
 // Display 5 Day forecast
 let displayFiveDay = function (forecastObj) {
   let days = forecastObj.daily;
 
-  console.log("----------");
-  for (let i = 0; i < days.length; i++) {
-    let theDate = moment(days[i].dt_txt).format("M/DD/YYYY H");
-    console.log(theDate);
+  // Just need days 1 - 5 from the 7 day forecast
+  for (let i = 1; i <= 5; i++) {
+    let currentCard = "#forecast" + i;
+
+    let theDate = moment(parseInt(days[i].dt) * 1000).format("M/DD/YYYY");
     let weatherIcon = days[i].weather[0].icon;
-    console.log("http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
     let temperature = days[i].temp.day;
-    console.log("Temperature: " + temperature + " °F");
     let humidity = days[i].humidity;
-    console.log("Humidity: " + humidity + "%");
-    console.log("----------");
+
+    $(currentCard).find(".card-title").text(theDate);
+    $(currentCard).find(".wi").attr("src", "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+    $(currentCard).find(".temp").text(temperature);
+    $(currentCard).find(".hum").text(humidity);
+
+    // Show the 5 day forecast
+    $(".fiveDay").css("visibility", "visible");
   }
 };
 
-fetchWeather();
+// Hide today's and 5 day forecasts until there is data
+// There may have been a bad API call before this so reset the HTML to expect a good result
+let setupResults = function () {
+  // Hide today's and 5 day forecasts
+  $(".today").css("visibility", "hidden");
+  $(".fiveDay").css("visibility", "hidden");
+
+  // Set HTML for today's weather panel to anticipate a successful API call
+  $(".today").html(todayGoodResult);
+  $("#fiveDayCards").html(fiveDayGoodResult);
+};
+
+// Search button clicked
+$("#citySearchBtn").click(function () {
+
+  // Get city to find the weather for
+  let city = $(this).siblings("#cityInput").val();
+
+  // Query the city's weather
+  fetchWeather(city);
+
+  // Show Results panel
+  setupResults();
+});
